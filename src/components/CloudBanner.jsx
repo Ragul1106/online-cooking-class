0a57a4bb27f6e039ce8a266c5ebcc8e3fd15fd92
Ragul116 <ragulranjith1106@gmail.com>
2025-07-31 21:24:54 +0530

    add vercel file

--- src/components/CloudBanner.jsx
+++ src/components/CloudBanner.jsx
@@ -42,11 +42,11 @@ function BakeryBanner() {
                     alt="Main Text Overlay"
                 />
 
-                <p className="absolute left-2 md:left-8 lg:left-8 top-20 md:top-40 lg:top-40  text-white font-medium text-sm md:text-base lg:text-2xl z-10">
+                <p className="absolute left-2 md:left-8 lg:left-8 top-24 md:top-40 lg:top-40  text-white font-medium text-xl md:text-base lg:text-2xl z-10">
                     Even If You Don't Have Any Prior Experience In Baking!
                 </p>
 
-                <div className="absolute left-5 md:left-36 top-32 md:top-48 lg:top-52 text-white text-left space-y-4 text-sm md:text-base lg:text-xl z-10 leading-relaxed mt-2 md:mt-4 lg:mt-10">
+                <div className="absolute left-5 md:left-36 top-44 md:top-48 lg:top-52 text-white text-left space-y-4 text-3xl md:text-base lg:text-xl z-10 leading-relaxed mt-2 md:mt-4 lg:mt-10">
                     <p className="flex items-center gap-3">
                         <img src={Batch} alt="Batch Icon" className="w-6 h-6" />
                         Batch II Start Date - 31st July 2025
@@ -65,29 +65,29 @@ function BakeryBanner() {
                     </p>
                 </div>
 
-                <div className="absolute left-20 md:left-32 top-[20rem] md:top-[28rem] text-white font-bold  text-sm md:text-sm lg:text-lg z-10">
+                <div className="absolute text-3xl left-20 md:left-32 top-[30rem] md:top-[28rem] text-white font-bold md:text-sm lg:text-lg z-10">
                     Program Fees: ₹ 1 Lakh | INR <br />
                     <span className="text-white-300 text-xl sm:text-4xl">36,000/-</span>
                 </div>
 
                 <button 
                 onClick={() => navigate("/enroll")}
-                className="absolute left-20 md:left-56 lg:left-56 bottom-[220px] md:bottom-[160px] lg:bottom-32 z-20">
-                    <img src={enrollNow} className="w-24 md:w-28 lg:w-48" alt="Enroll Now" />
+                className="absolute left-20 md:left-56 lg:left-56 bottom-[320px] md:bottom-[160px] lg:bottom-32 z-20">
+                    <img src={enrollNow} className="w-34 md:w-28 lg:w-48" alt="Enroll Now" />
                 </button>
 
 
                 <img
                     src={cheffimg}
                     alt="Chef"
-                    className="absolute right-2 md:right-4 lg:right-6 bottom-[400px] md:bottom-[220px] lg:bottom-28 
-             w-20 md:w-40 lg:w-[400px] 
-             h-30 md:h-40 lg:h-[400px] 
+                    className="absolute right-44 md:right-4 lg:right-6 bottom-[430px] md:bottom-[220px] lg:bottom-28 
+             w-56 md:w-40 lg:w-[400px] 
+             h-56 md:h-40 lg:h-[400px] 
              rounded-full border-4 border-white object-cover z-10"
                 />
 
 
-                <p className="absolute bottom-[280px]  md:bottom-[118px] lg:bottom-20 left-0 md:left-24 text-sm md:text-base lg:text-lg text-red-500 z-10 bg-white px-1 md:px-4 py-1">
+                <p className="absolute bottom-[280px]  md:bottom-[118px] lg:bottom-20 left-20 md:left-24 text-sm md:text-base lg:text-lg text-red-500 z-10 bg-white px-1 md:px-4 py-1">
                     We're looking for Action Takers. Program open for 50 participants only.
                 </p>
 
